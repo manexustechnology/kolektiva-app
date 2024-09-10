5c18ff7072f908f2c4f8ad89be8df2247aa060a6
David Ridwan <ddavidridwan@gmail.com>
2024-09-11 05:43:02 +0700

    feat(referral): auth integration, user referral and point

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import Navbar from "../components/Navbar";
 import Footer from "@/components/Footer";
 import { useState } from "react";
 import { interGlobalFont } from "@/commons/font";
+import { Toaster } from 'react-hot-toast';
 
 export default function RootLayout({
   children,
@@ -17,6 +18,7 @@ export default function RootLayout({
       <body
         className={`${interGlobalFont.className} flex flex-col min-h-screen`}
       >
+        <Toaster position="bottom-left" />
         <Providers>
           <Navbar />
           <div
